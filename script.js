ae082f49053a0ba22982b82a23dca6e4515614cf
IDKA <pubgpenguin2018@gmail.com>
2025-02-25 22:30:36 +0530

    load

--- script.js
+++ script.js
@@ -67,3 +67,8 @@ window.addEventListener('load', () => {
         behavior: 'smooth'
     });
 });
+
+window.onload = function() {
+    // Reset the form fields when the page loads
+    document.getElementById("form").reset();
+};
